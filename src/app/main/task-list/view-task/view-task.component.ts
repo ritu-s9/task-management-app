f1bc5cf9b69adc627e5fdeeacf58f08bb5ec2426
ritu-s9 <shomeguddy@gmail.com>
2024-07-25 17:46:07 +0530

    feat:task view implemented.

--- src/app/main/task-list/view-task/view-task.component.ts
+++ src/app/main/task-list/view-task/view-task.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { DialogService, DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 @Component({
   selector: 'app-view-task',
   templateUrl: './view-task.component.html',
   styleUrls: ['./view-task.component.scss']
 })
 export class ViewTaskComponent {
+  dialogData: any;
+  constructor(public ref : DynamicDialogRef,public config: DynamicDialogConfig){
+    this.dialogData = this.config.data.rawData;
+  }
 
 }
